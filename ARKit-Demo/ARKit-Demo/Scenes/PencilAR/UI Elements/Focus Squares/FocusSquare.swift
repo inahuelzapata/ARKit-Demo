2217f27c4a342bc4490b98c896b56ee7b911241a
Nahuel Zapata <nahuel.zapata@fdvsolutions.com>
2018-04-04 21:56:22 -0300

    Swiftlin issues solved

--- ARKit-Demo/ARKit-Demo/Scenes/PencilAR/UI Elements/Focus Squares/FocusSquare.swift
+++ ARKit-Demo/ARKit-Demo/Scenes/PencilAR/UI Elements/Focus Squares/FocusSquare.swift
@@ -11,44 +11,27 @@ import Foundation
 /// - Tag: FocusSquare
 class FocusSquare: SCNNode {
 
-    // MARK: - Focus Square Configuration Properties
-
-    // Original size of the focus square in m.
     private let focusSquareSize: Float = 0.17
 
-    // Thickness of the focus square lines in m.
     private let focusSquareThickness: Float = 0.018
 
-    // Scale factor for the focus square when it is closed, w.r.t. the original size.
     private let scaleForClosedSquare: Float = 0.97
 
-    // Side length of the focus square segments when it is open (w.r.t. to a 1x1 square).
     private let sideLengthForOpenSquareSegments: CGFloat = 0.2
 
-    // Duration of the open/close animation
     private let animationDuration = 0.7
 
-    // Color of the focus square
-    static let primaryColor = #colorLiteral(red: 1, green: 0.8, blue: 0, alpha: 1) // base yellow
-    static let primaryColorLight = #colorLiteral(red: 1, green: 0.9254901961, blue: 0.4117647059, alpha: 1) // light yellow
-
-    // For scale adapdation based on the camera distance, see the `scaleBasedOnDistance(camera:)` method.
-
-    // MARK: - Position Properties
+    static let primaryColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+    static let primaryColorLight = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
 
     var lastPositionOnPlane: float3?
     var lastPosition: float3?
 
-    // MARK: - Other Properties
-
     private var isOpen = false
     private var isAnimating = false
 
-    // use average of recent positions to avoid jitter
     private var recentFocusSquarePositions: [float3] = []
     private var anchorsOfVisitedPlanes: Set<ARAnchor> = []
-
-    // MARK: - Initialization
 
     override init() {
         super.init()
@@ -60,8 +43,6 @@ class FocusSquare: SCNNode {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    // MARK: - Appearence
 
     func update(for position: float3, planeAnchor: ARPlaneAnchor?, camera: ARCamera?) {
         lastPosition = position
@@ -294,17 +275,6 @@ class FocusSquare: SCNNode {
     }()
 
     private lazy var focusSquareNode: SCNNode = {
-        /*
-         The focus square consists of eight segments as follows, which can be individually animated.
-         
-         s1  s2
-         _   _
-         s3 |     | s4
-         
-         s5 |     | s6
-         -   -
-         s7  s8
-         */
         let s1 = Segment(name: "s1", corner: .topLeft, alignment: .horizontal)
         let s2 = Segment(name: "s2", corner: .topRight, alignment: .horizontal)
         let s3 = Segment(name: "s3", corner: .topLeft, alignment: .vertical)
